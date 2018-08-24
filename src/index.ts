905e52ebf2c60b2b2586889fdfd221c4a29f7dee
Andrew Pashinnik <tobirawork@gmail.com>
2018-08-24 19:47:48 +0300

    feat(typings): add export typings

---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { generate, handleField } from './generator'
---
+++ swoody.d.ts
@@ -0,0 +1,4 @@
+declare namespace swoody {
+    export function generate(schema: any): string
+    export function handleField(field: any): any
+}
