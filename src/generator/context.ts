43f7e6863be8005869d3fa4c42222bebcb8b3d0d
Andrew Pashinnik <tobirawork@gmail.com>
2018-08-25 17:44:10 +0300

    refactor(generator/context): cleaning from unused shit

--- src/generator/context.ts
+++ src/generator/context.ts
@@ -1,24 +1,8 @@
-import { getRandomInRange } from './utils'
 import * as fakerApi from 'faker'
 import * as momentApi from 'moment'
 
 export const faker = fakerApi
 export const moment = momentApi
-
-export function objectId() {
-    // @todo make it dynamic
-    return '_1asdf234jksjdhf823hf9un29d'
-}
-
-export function boolean(): boolean {
-    return Math.random() >= 0.5
-}
-
-export function integer(min?: number, max?: number): number {
-    if (!min && !max) return getRandomInRange(-100000000, 100000000)
-
-    return getRandomInRange(min, max)
-}
 
 export function same(argument: any): any {
     return argument
