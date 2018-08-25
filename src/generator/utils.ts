52cc42677ba129ba85564bdfa54d3069f2d1264d
Andrew Pashinnik <tobirawork@gmail.com>
2018-08-25 17:43:22 +0300

    refactor(generator/utils): mustache regex and getRepeatTimes function

--- src/generator/utils.ts
+++ src/generator/utils.ts
@@ -1,14 +1,15 @@
-export const MUSTASCHE_REGEX = /{{([^{}]+)}}/gi
+export const MUSTASCHE_REGEX = /{{(((?!}})(.|\n))*)}}/gi
 
 export function getRandomInRange(min: number, max: number) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export function getRepeatTimes(times: number | number[]) {
-    if (Array.isArray(times) && times.length >= 2) {
+export function getRepeatTimes(times: number | number[]): number {
+    if (Array.isArray(times) && times.length === 2) {
         const [min, max] = times
         return getRandomInRange(min, max)
     } else {
+        if (Array.isArray(times)) return times[0] ? times[0] : 0
         return times
     }
 }
