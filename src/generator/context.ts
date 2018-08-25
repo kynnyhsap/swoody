3f0139df5d1bfe3f6320315b7af46859f7e990fc
Andrew Pashinnik <tobirawork@gmail.com>
2018-08-25 18:10:25 +0300

    refactor(context)

--- src/generator/context.ts
+++ src/generator/context.ts
@@ -3,7 +3,4 @@ import * as momentApi from 'moment'
 
 export const faker = fakerApi
 export const moment = momentApi
-
-export function same(argument: any): any {
-    return argument
-}
+export const sameValue = (argument: any) => argument
