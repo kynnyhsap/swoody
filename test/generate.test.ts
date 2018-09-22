68599ff0f25a9dce3e9ad47f47be27d0635f4004
Andrew Pashinnik <tobirawork@gmail.com>
2018-09-22 16:36:35 +0300

    test: update

---
+++ test/generate.test.ts
@@ -0,0 +1,149 @@
+import { generate } from '../src/generator'
+
+const tests = [
+    {
+        name: 'should return stringified null',
+        expected: 'null',
+        value: null,
+    },
+    {
+        name: 'should return undefined',
+        expected: undefined,
+        value: undefined,
+    },
+    {
+        name: 'should return stringified number',
+        expected: '10',
+        value: 10,
+    },
+    {
+        name: 'should return stringified object',
+        expected: '{"kek":10}',
+        value: { kek: 10 },
+    },
+]
+
+describe('generate function', () => {
+    tests.forEach(tt => {
+        it(tt.name, () => {
+            const received = generate(tt.value)
+
+            expect(received).toEqual(tt.expected)
+        })
+    })
+
+    //#region k
+    // describe('number', () => {
+    //     it('should return number', () => {
+    //         expect(handleField(12312312)).toBe(12312312)
+    //         expect(handleField(-23542)).toBe(-23542)
+    //         expect(handleField(0)).toBe(0)
+    //     })
+
+    //     it('should return null if value equals NaN', () => {
+    //         expect(handleField(NaN)).toBeNull()
+    //     })
+
+    //     it('should return null if value equals Infinity', () => {
+    //         expect(handleField(Infinity)).toBeNull()
+    //         expect(handleField(-Infinity)).toBeNull()
+    //     })
+    // })
+
+    // describe('string', () => {
+    //     it('should return simple string', () => {
+    //         expect(handleField('Hello')).toBe('Hello')
+    //     })
+
+    //     it('should return evaluated string', () => {
+    //         expect(handleField('{{"kek".toUpperCase()}}')).toBe('KEK')
+    //         expect(handleField('lol {{sameValue(10)}} _ {{sameValue(3)}}')).toBe('lol 10 _ 3')
+    //     })
+
+    //     it('should return evaluated value', () => {
+    //         expect(handleField('{{sameValue(10)}}')).toBe(10)
+    //         expect(handleField('{{10 * 5}}')).toBe(50)
+    //         expect(handleField('{{sameValue(null)}}')).toBe(null)
+    //     })
+    // })
+
+    // describe('function', () => {
+    //     it('should call function and return value', () => {
+    //         const fun = (context: any) => {
+    //             const str = 'l' + 'o' + 'l'
+
+    //             return context.sameValue(str).toUpperCase()
+    //         }
+
+    //         expect(handleField(fun)).toBe('LOL')
+    //     })
+
+    //     it('should call function and return null', () => {
+    //         const fun = (context: any) => {
+    //             1 + 1
+    //         }
+
+    //         expect(handleField(fun)).toBeNull()
+    //     })
+    // })
+
+    // describe('object', () => {
+    //     it('should return array of values', () => {
+    //         const field = '{{sameValue("l")}}'
+
+    //         expect(
+    //             handleField({
+    //                 repeat: 3,
+    //                 object: field,
+    //             })
+    //         ).toEqual(['l', 'l', 'l'])
+
+    //         for (let i = 0; i < 10; i++) {
+    //             expect(handleField({ repeat: [3, 10], object: field }).length).toBeGreaterThanOrEqual(3)
+    //             expect(handleField({ repeat: [3, 10], object: field }).length).toBeLessThanOrEqual(10)
+    //         }
+    //     })
+
+    //     it('should return empty array', () => {
+    //         expect(handleField({ repeat: 0, object: {} })).toEqual([])
+    //         expect(handleField({ repeat: -1, object: {} })).toEqual([])
+    //     })
+
+    //     it('should return evaluated object', () => {
+    //         const object = {
+    //             age: 12,
+    //             kek: '{{3+10}}',
+    //             name: {
+    //                 first: '{{sameValue("Bob")}}',
+    //                 last: '{{sameValue("Marley")}}',
+    //             },
+    //         }
+
+    //         const resulted = {
+    //             age: 12,
+    //             kek: 13,
+    //             name: {
+    //                 first: 'Bob',
+    //                 last: 'Marley',
+    //             },
+    //         }
+
+    //         expect(handleField(object)).toEqual(resulted)
+    //     })
+    // })
+
+    // describe('array', () => {
+    //     it('should return array of evaluated values', () => {
+    //         const arr = [1, '{{sameValue("30")}}', 4, 234]
+
+    //         expect(handleField(arr)).toEqual([1, '30', 4, 234])
+    //     })
+    // })
+
+    // describe('undefined', () => {
+    //     it('should return undefined', () => {
+    //         expect(handleField(undefined)).toBeUndefined()
+    //     })
+    // })
+    //#endregion k
+})
--- test/handleField.test.ts
+++ test/handleField.test.ts
@@ -2,121 +2,243 @@ import { handleField } from '../src/generator'
 
 describe('handleField function', () => {
     describe('number', () => {
-        it('should return number', () => {
-            expect(handleField(12312312)).toBe(12312312)
-            expect(handleField(-23542)).toBe(-23542)
-            expect(handleField(0)).toBe(0)
-        })
+        const tests = [
+            {
+                name: 'should return number',
+                expected: 12312312,
+                value: 12312312,
+            },
+            {
+                name: 'should return number',
+                expected: -2312,
+                value: -2312,
+            },
+            {
+                name: 'should return number',
+                expected: 0,
+                value: 0,
+            },
+            {
+                name: 'should return null',
+                expected: null,
+                value: NaN,
+            },
+            {
+                name: 'should return null',
+                expected: null,
+                value: Infinity,
+            },
+            {
+                name: 'should return null',
+                expected: null,
+                value: -Infinity,
+            },
+        ]
 
-        it('should return null if value equals NaN', () => {
-            expect(handleField(NaN)).toBeNull()
-        })
+        tests.forEach(tt => {
+            it(tt.name, () => {
+                const received = handleField(tt.value, {})
 
-        it('should return null if value equals Infinity', () => {
-            expect(handleField(Infinity)).toBeNull()
-            expect(handleField(-Infinity)).toBeNull()
+                expect(received).toEqual(tt.expected)
+            })
         })
     })
 
     describe('string', () => {
-        it('should return simple string', () => {
-            expect(handleField('Hello')).toBe('Hello')
-        })
+        const tests = [
+            {
+                name: 'should return string',
+                expected: 'Hello',
+                value: 'Hello',
+            },
+            {
+                name: 'should return evaluated string',
+                expected: 'KEK',
+                value: '{{"kek".toUpperCase()}}',
+            },
+            {
+                name: 'should return evaluated string',
+                expected: 'lol 10 _ 3',
+                value: 'lol {{sameValue(10)}} _ {{sameValue(3)}}',
+            },
+            {
+                name: 'should return evaluated value',
+                expected: 10,
+                value: '{{ sameValue(10) }}',
+            },
+            {
+                name: 'should return evaluated value',
+                expected: 50,
+                value: '{{ 10 * 5 }}',
+            },
+            {
+                name: 'should return evaluated value',
+                expected: null,
+                value: '{{ sameValue(null) }}',
+            },
+        ]
 
-        it('should return evaluated string', () => {
-            expect(handleField('{{"kek".toUpperCase()}}')).toBe('KEK')
-            expect(handleField('lol {{sameValue(10)}} _ {{sameValue(3)}}')).toBe('lol 10 _ 3')
-        })
+        tests.forEach(tt => {
+            it(tt.name, () => {
+                const received = handleField(tt.value, {})
 
-        it('should return evaluated value', () => {
-            expect(handleField('{{sameValue(10)}}')).toBe(10)
-            expect(handleField('{{10 * 5}}')).toBe(50)
-            expect(handleField('{{sameValue(null)}}')).toBe(null)
+                expect(received).toEqual(tt.expected)
+            })
         })
     })
 
     describe('function', () => {
-        it('should call function and return value', () => {
-            const fun = (context: any) => {
-                const str = 'l' + 'o' + 'l'
+        const tests = [
+            {
+                name: 'should call function and return value',
+                expected: 'LOL',
+                value: (context: any) => {
+                    const str = 'l' + 'o' + 'l'
 
-                return context.sameValue(str).toUpperCase()
-            }
+                    return context.sameValue(str).toUpperCase()
+                },
+            },
+            {
+                name: 'should call function and return null',
+                expected: null,
+                value: (context: any) => {
+                    1 + 1
+                },
+            },
+        ]
 
-            expect(handleField(fun)).toBe('LOL')
-        })
+        tests.forEach(tt => {
+            it(tt.name, () => {
+                const received = handleField(tt.value, {})
 
-        it('should call function and return null', () => {
-            const fun = (context: any) => {
-                1 + 1
-            }
-
-            expect(handleField(fun)).toBeNull()
+                expect(received).toEqual(tt.expected)
+            })
         })
     })
 
     describe('object', () => {
         it('should return array of values', () => {
-            const field = '{{sameValue("l")}}'
-
-            expect(
-                handleField({
-                    repeat: 3,
-                    object: field,
-                })
-            ).toEqual(['l', 'l', 'l'])
-
             for (let i = 0; i < 10; i++) {
-                expect(handleField({ repeat: [3, 10], object: field }).length).toBeGreaterThanOrEqual(3)
-                expect(handleField({ repeat: [3, 10], object: field }).length).toBeLessThanOrEqual(10)
+                expect(handleField({ repeat: [3, 10], object: '{{sameValue("l")}}' }, {}).length).toBeGreaterThanOrEqual(3)
+                expect(handleField({ repeat: [3, 10], object: '{{sameValue("l")}}' }, {}).length).toBeLessThanOrEqual(10)
             }
         })
 
-        it('should return empty array', () => {
-            expect(handleField({ repeat: 0, object: {} })).toEqual([])
-            expect(handleField({ repeat: -1, object: {} })).toEqual([])
-        })
-
-        it('should return evaluated object', () => {
-            const object = {
-                age: 12,
-                kek: '{{3+10}}',
-                name: {
-                    first: '{{sameValue("Bob")}}',
-                    last: '{{sameValue("Marley")}}',
+        const tests = [
+            {
+                name: 'should return array of values',
+                expected: ['l', 'l', 'l'],
+                value: {
+                    repeat: 3,
+                    object: '{{sameValue("l")}}',
                 },
-            }
-
-            const resulted = {
-                age: 12,
-                kek: 13,
-                name: {
-                    first: 'Bob',
-                    last: 'Marley',
+            },
+            {
+                name: 'should return empty array',
+                expected: [],
+                value: { repeat: 0, object: {} },
+            },
+            {
+                name: 'should return empty array',
+                expected: [],
+                value: { repeat: -1, object: {} },
+            },
+            {
+                name: 'should return empty array',
+                expected: [],
+                value: { repeat: -134, object: {} },
+            },
+            {
+                name: 'should return evaluated object',
+                expected: {
+                    age: 12,
+                    kek: 13,
+                    name: {
+                        first: 'Bob',
+                        last: 'Marley',
+                    },
                 },
-            }
+                value: {
+                    age: 12,
+                    kek: '{{3+10}}',
+                    name: {
+                        first: '{{sameValue("Bob")}}',
+                        last: '{{sameValue("Marley")}}',
+                    },
+                },
+            },
+        ]
 
-            expect(handleField(object)).toEqual(resulted)
+        tests.forEach(tt => {
+            it(tt.name, () => {
+                const received = handleField(tt.value, {})
+
+                expect(received).toEqual(tt.expected)
+            })
         })
     })
 
     describe('array', () => {
-        it('should return array of evaluated values', () => {
-            const arr = [1, '{{sameValue("30")}}', 4, 234]
+        const tests = [
+            {
+                name: 'should return array of evaluated values',
+                expected: [1, '30', 4, 234, { kek: 'lol' }],
+                value: [
+                    1,
+                    '{{sameValue("30")}}',
+                    4,
+                    234,
+                    {
+                        kek() {
+                            return 'lol'
+                        },
+                    },
+                ],
+            },
+        ]
 
-            expect(handleField(arr)).toEqual([1, '30', 4, 234])
+        tests.forEach(tt => {
+            it(tt.name, () => {
+                const received = handleField(tt.value, {})
+
+                expect(received).toEqual(tt.expected)
+            })
         })
     })
 
     describe('null', () => {
-        it('should return null', () => {
-            expect(handleField(null)).toBeNull()
+        const tests = [
+            {
+                name: 'should return null',
+                expected: null,
+                value: null,
+            },
+        ]
+
+        tests.forEach(tt => {
+            it(tt.name, () => {
+                const received = handleField(tt.value, {})
+
+                expect(received).toEqual(tt.expected)
+            })
         })
     })
 
     describe('undefined', () => {
-        it('should return undefined', () => {
-            expect(handleField(undefined)).toBeUndefined()
+        const tests = [
+            {
+                name: 'should return undefined',
+                expected: undefined,
+                value: undefined,
+            },
+        ]
+
+        tests.forEach(tt => {
+            it(tt.name, () => {
+                const received = handleField(tt.value, {})
+
+                expect(received).toEqual(tt.expected)
+            })
         })
     })
 })
